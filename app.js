7196462dff95da91610a699436a4870ffda5be4a
bekad <abexiocdesic@gmail.com>
2026-08-21 10:16:27 +0300

    éxpress server intialized

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+
+
+
+
+
+
+
+// start app on port 1111
+
+
+app.listen(1111,()=>{
+    console.log(`NODE  SERVER STARTED ON PORT` + 1111);
+    
+})
